8055e67eca6dc2df96c87630fcf7b7b6a7113f8d
Jose Gerstner Javid <joseernestogerstnerjavid@gmail.com>
2018-09-09 20:48:46 -0300

    Agregando tp

---
+++ spcpd/src/tps/AlgoritmoDeTurno.java
@@ -0,0 +1,14 @@
+package tps;
+/* 
+ * Crear rutina que implemente el "algoritmo de turno" (p y q)
+ * loop forever = 1000 veces
+ * ver que cumpla las 4 propiedades (exclusión mutua, deadlock, inanición, contención) 
+ */
+public class AlgoritmoDeTurno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
